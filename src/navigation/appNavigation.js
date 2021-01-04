48171f9846f64bc15145c5b71b33a8f0f1ab72ff
Elin Alm <elin95alm@gmail.com>
2021-01-04 15:37:58 +0100

    BottomNavigator

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomePage from '../scenes/homePage';
+import { NavigationContainer } from '@react-navigation/native';
 import { LandingPage } from '../scenes/landingPage';
 import DetailCard from '../components/detailCard';
+import { StyleSheet } from 'react-native';
+import { TabNavigator } from '../navigation/bottomNavigator';
+import HomePage from '../scenes/homePage';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 
 const { Navigator, Screen } = createStackNavigator();
 
 const HomeNavigator = () => (
   <Navigator headerMode='none' initialRouteName="LandingPage">
-    <Screen name='HomePage' component={HomePage}/>
     <Screen name='LandingPage' component={LandingPage}/>
+    <Screen name='HomePage' component={TabNavigator}/>
     <Screen name="DetailPage" component={DetailCard}/>
   </Navigator>
 );
+
 
 export const AppNavigator = () => (
   <NavigationContainer>
     <HomeNavigator/>
   </NavigationContainer>
 );
+
+const styles = StyleSheet.create({
+  BottomNavigation: {
+      marginVertical: 8,
+  },
+  icons: {
+    color: 'white',
+  }
+})
