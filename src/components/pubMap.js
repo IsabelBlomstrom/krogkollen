340bea2358483e9ead70505ddccd210f7cae51e7
Isabel Blomström <isabel.blomstrom@gmail.com>
2021-01-08 11:13:10 +0100

    custom marker and tooltip added

--- src/components/pubMap.js
+++ src/components/pubMap.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Layout } from '@ui-kitten/components';
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, Image } from 'react-native';
 import { default as theme } from '../../AppTheme.json';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Callout, Marker } from 'react-native-maps';
 
 
 export default function PubMap() {
@@ -17,19 +17,26 @@ export default function PubMap() {
       longitudeDelta: 0.0421,
     }}
   >
-    <Marker
-    coordinate={{
-      latitude: 57.71250,
-      longitude: 11.991
-    }}
-    />
+      <Marker
+      title="Tullen Lejonet"
+      coordinate={{
+        latitude: 57.71250,
+        longitude: 11.991
+      }}
+      >
+         <Image source={require('../assets/images/marker.png')} style={{height: 40, width:25 }} />
+
+        </Marker>
+
         <Marker
+        title="Ölkompaniet"
     coordinate={{
       latitude: 57.71490,
       longitude: 12.0045
     }}
     />
         <Marker
+        title="Lilla restaurangen"
     coordinate={{
       latitude: 57.71470,
       longitude: 11.998
@@ -52,5 +59,10 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginVertical: 40,
     height: Dimensions.get('window').height/2,
+  },
+  callout: {
+    width: 100,
+    height: 50,
+
   }
 })
